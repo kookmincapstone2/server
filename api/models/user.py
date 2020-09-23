a7770407ceacc67d20a9030a00fa906e7e99dde1
yeonsookim40282628 <gjb2628@gmail.com>
2020-09-24 00:14:08 +0900

    Room과 관계지어줌

--- api/models/user.py
+++ api/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from db import Base
 
@@ -12,3 +13,4 @@ class User(Base):
     name = Column(String, nullable=False)
     student_id = Column(Integer, nullable=False)
     phone = Column(String, unique=True, nullable=False)
+    room = relationship('Room', back_populates='user')
