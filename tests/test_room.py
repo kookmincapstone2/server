2a53f8caaa0b2b764078d413be7618585d5100ec
yeonsookim40282628 <gjb2628@gmail.com>
2020-10-03 16:05:19 +0900

    get_room_management test code 추가

--- tests/test_room.py
+++ tests/test_room.py
@@ -6,3 +6,12 @@ def test_post_room_management(client, user):
 
     res = client.post('/api/room/management', data=data)
     assert res.status_code == 200
+
+
+def test_get_room_management(client, user):
+    data = {
+        'user_id': user.id,
+    }
+
+    res = client.get('/api/room/management', query_string=data)
+    assert res.status_code == 200
