f80d946850c9ac8c36ff5aca0016b98ae12eba5c
yeonsookim40282628 <gjb2628@gmail.com>
2020-10-09 13:53:15 +0900

    방 가입 및 탈퇴 함수 작성

--- api/room.py
+++ api/room.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from werkzeug.exceptions import BadRequest, NotFound, Forbidden
+from werkzeug.exceptions import NotFound, Forbidden, Unauthorized, Conflict
 
 from api.models.room import *
 from api.models.user import User
@@ -86,7 +86,7 @@ def put_room_management(data, db):  # 방 정보 수정
 
 @app.route('/room/management', methods=['DELETE'])
 @api
-def delete_room_management(data, db):
+def delete_room_management(data, db):  # 방 제거 함수
     req_list = ['user_id', 'room_id']
     check_data(data, req_list)
 
@@ -98,7 +98,7 @@ def delete_room_management(data, db):
     if not str(room.master_id) == data['user_id']:  # 해당 유저가 마스터가 아니어서 권한이 없음
         raise Forbidden
 
-    db.delete(room)
+    room.deleted_on = datetime.datetime.now()
     db.commit()
 
     return jsonify({})
@@ -106,7 +106,57 @@ def delete_room_management(data, db):
 
 @app.route('/room/member/management', methods=['POST'])
 @api
-def post_room_member_management(data, db):
-    req_list = ['room_id']
+def post_room_member_management(data, db):  # 방 가입 함수
+    req_list = ['user_id', 'room_id', 'invite_code']
     check_data(data, req_list)
+
+    user = db.query(User).filter(User.id == data['user_id']).first()
+    if not user:  # 해당 유저 존재하지 않음
+        raise NotFound
+
+    room = db.query(Room).filter(Room.id == data['room_id']).first()
+    if not room:  # 해당 방 존재하지 않음
+        raise NotFound
+
+    room_member = db.query(RoomMember).filter(RoomMember.room_id == data['room_id'],
+                                              RoomMember.member_id == data['user_id'],
+                                              RoomMember.deleted_on.is_(None), ).first()
+    if room_member:  # 이미 가입된 방
+        raise Conflict
+
+    if not room.invite_code == data['invite_code']:  # 초대코드가 맞지 않음
+        raise Unauthorized
+
+    new_room_member = RoomMember(room_id=data['room_id'],
+                                 member_id=data['user_id'], )
+    db.add(new_room_member)
+    db.commit()
+
+    return jsonify({})
+
+
+@app.route('/rom/member/management', methods=['DELETE'])
+@api
+def delete_room_member_management(data, db):
+    req_list = ['user_id', 'room_id']
+    check_data(data, req_list)
+
+    user = db.query(User).filter(User.id == data['user_id']).first()
+    if not user:  # 해당 유저 존재하지 않음
+        raise NotFound
+
+    room = db.query(Room).filter(Room.id == data['room_id']).first()
+    if not room:  # 해당 방 존재하지 않음
+        raise NotFound
+
+    room_member = db.query(RoomMember).filter(RoomMember.room_id == data['room_id'],
+                                              RoomMember.member_id == data['user_id'],
+                                              RoomMember.deleted_on.is_(None), ).first()
+
+    if not room_member:  # 가입되지 않은 방
+        raise NotFound
+
+    room_member.deleted_on = datetime.datetime.now()
+    db.commit()
+
     return jsonify({})
